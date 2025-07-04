2d2472499d41e1eb0c8f887f7dc7ab9a2197a13e
FranBernardes <franiasd7@gmail.com>
2025-07-04 18:57:45 -0300

    inclui a URL no cypressconfig

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    defaultCommandTimeout: 10000,
+    baseUrl: "https://automationpratice.com.br",
+    
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -4,7 +4,7 @@ describe("Login", () => {
 
   it("Acessar Login", () => {
     //acessar a pagina
-    cy.visit("https://automationpratice.com.br/login");
+    cy.visit("/login");
     //preencher os campos
     cy.get("#user").type("fran@tester.com");
     cy.get("#password").type(123456);
@@ -13,37 +13,35 @@ describe("Login", () => {
     //validar mensagem
     cy.get("#swal2-title").should("contain", "Login realizado");
   });
-  it("Login com seha inválida", () => {
+  it("Login com senha inválida", () => {
     //acessar a pagina
-    cy.visit("https://automationpratice.com.br/login");
+    cy.visit("/login");
     //preencher os campos
     cy.get("#user").type("fran@tester.com");
-    cy.get("#password").click();
+    cy.get("#password").type(123);
     //clicar em Login
     cy.get("#btnLogin").click();
     //validar mensagem
-    cy.get(
-      "#login_area > div > div > div > div > div:nth-child(3) > span"
-    ).should("contain", "Senha inválida.");
+    cy.get('.invalid_input')
+    .should("contain", "Senha inválida.");
   });
 
   it("Login com email inválida", () => {
     //acessar a pagina
-    cy.visit("https://automationpratice.com.br/login");
+    cy.visit("/login");
     //preencher os campos
     cy.get("#user").click();
     cy.get("#password").type(123456);
     //clicar em Login
     cy.get("#btnLogin").click();
     //validar mensagem
-    cy.get(
-      "#login_area > div > div > div > div > div:nth-child(2) > span"
-    ).should("contain", "E-mail inválido.");
+   cy.get('.invalid_input')
+   .should("contain", "E-mail inválido.");
   });
 
   it("Login com campos vazios", () => {
     //acessar a pagina
-    cy.visit("https://automationpratice.com.br/login");
+    cy.visit("/login");
     //preencher os campos
     cy.get("#user").click();
     cy.get("#password").click();
